ff55c6f82e4c472ed7b0eb150d489b5d4221b157
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-08-05 19:14:05 +0330

    Add project files.

---
+++ DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+using MauiApp1.ViewModel;
+
+namespace MauiApp1
+
+{
+    public partial class DetailPage : ContentPage
+    {
+        public DetailPage(DetailViewModel vm)
+        {
+            InitializeComponent();
+
+            BindingContext = vm;
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MauiApp1.ViewModel
+{
+    public partial class MainViewModel :ObservableObject
+    {
+
+        
+        public MainViewModel()
+        {
+            items = new ObservableCollection<string>();
+        }
+        [ObservableProperty]
+        string text;
+
+        [ObservableProperty] 
+        ObservableCollection<string> items;
+
+        [RelayCommand]
+        void Add()
+        {
+            if(string.IsNullOrEmpty(text))
+                return;
+
+            items.Add(text);
+
+            Text = string.Empty;
+        }
+
+        [RelayCommand]
+        void Delete(string text)
+
+        {
+            if (items.Contains(text))
+
+                items.Remove(text);
+        }
+
+        [RelayCommand]
+
+        async Task Tap(string s)
+        {
+            await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+        }
+
+    }
+}
